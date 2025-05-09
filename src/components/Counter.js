e3180b09ea80c12f29ad598648e9108bf638a264
Pankaj Kumar <pankaj.hzb@gmail.com>
2025-05-09 08:26:35 +0530

    condn

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,17 +4,25 @@ function Counter() {
     const [counter, setCounter] = useState(0);
     const handleClick1 =()=>{
         setCounter(counter + 1);
+        if(counter >= 10) {
+            alert("You have reached the maximum count of 10!");
+            return;
+        }
     }
 
     const handleClick2 = () => {
         setCounter(counter - 1)
+        if(counter <= 0) {
+            alert("You have reached the minimum count of 0!");
+            return;
+        }
     }
 
 return (
     <div>
     <p>Count: {counter}</p>
-    <button onClick = {handleClick1}>ClickMe</button>
-    <button onClick = {handleClick2}>ClickMe</button>
+    <button onClick = {handleClick1}>AddMe</button>
+    <button onClick = {handleClick2}>SubtractMe</button>
     </div>
 );
 }
